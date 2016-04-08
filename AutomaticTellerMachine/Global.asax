e041d7cdb6bd44ab260819b818cea3a686e0db03
thyrobots <thyappid@gmail.com>
2016-04-08 16:34:49 -0700

    Add project files.

---
+++ AutomaticTellerMachine/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutomaticTellerMachine.MvcApplication" Language="C#" %>
---
+++ AutomaticTellerMachine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutomaticTellerMachine.Startup))]
+namespace AutomaticTellerMachine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
